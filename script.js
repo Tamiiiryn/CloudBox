2babe52ef76d34e47ccb773385345e469d598ce9
Tamiiiryn <Nurulutami024@gmail.com>
2026-07-15 21:47:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function login() {
     window.location.href = "dashboard.html";
 }
+
+function uploadFile() {
+    const input = document.getElementById("fileInput");
+
+    if (input.files.length === 0) {
+        alert("Pilih file terlebih dahulu.");
+        return;
+    }
+
+    const namaFile = input.files[0].name;
+
+    const daftar = document.getElementById("fileList");
+
+    const item = document.createElement("li");
+    item.textContent = namaFile;
+
+    daftar.appendChild(item);
+
+    alert("File berhasil ditambahkan ke daftar.");
+}
